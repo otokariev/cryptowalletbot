b19f3b377a4d1b75ba44023bdb9faeb41b5f74b1
otokariev <otokariev86@gmail.com>
2024-02-28 18:15:49 +0100

    update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-TOKEN = "6729432471:AAGfGD7EPU6eKY3RFX709lZNGfG-pZzkRos"  # whiami english teacher
-TOKEN2 = "5140624469:AAFb1IEahts-sLyX29XZHB0xyZ2VfHg6RDs"  # nixon2022 crypto wallet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.environ.get('TOKEN')
